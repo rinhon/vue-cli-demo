bbf951846d94399a42c76e884c0fe710029b37ab
Rinhon <54661549+rinhon@users.noreply.github.com>
2020-11-15 17:18:10 +0800

    芜湖,好起来了

---
+++ src/views/Login.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>登录页面</h1>
+        <router-link to="register">注册</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>注册</h1>
+        <router-link to="login">登录</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Register"
+    }
+</script>
+
+<style scoped>
+
+</style>
